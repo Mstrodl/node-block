c996ed1a9da162155c38809db42e66ce1079dba5
Jack <epicawesomewolfgaming@gmail.com>
2017-12-22 23:59:07 -0500

    new stuff

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ async function getDifficulty() {
     }
 }
 
+async function getBlockCount() {
+    try {
+        const response = await got('blockchain.info/q/getblockcount')
+        return response.body
+    } catch(err) {
+        return err
+    }
+}
+
 async function totalBtc() {
     try {
         const response = await got('blockchain.info/q/totalbc')
@@ -18,7 +27,43 @@ async function totalBtc() {
     }
 }
 
+async function bcPerBlock() {
+    try {
+        const response = await got('blockchain.info/q/bcperblock')
+        return response.body
+    } catch(err) {
+        return err
+    }
+}
+
+async function latestHash() {
+    try {
+        const response = await got('blockchain.info/q/latesthash')
+        return response.body
+    } catch(err) {
+        return err
+    }
+}
+
+
+// WIP
+// Seems to be broken to wallet reading -- will fix later
+
+// async function getWalletBalance(address) {
+//     try {
+//         const response = await got(`blockchain.info/balance?active=${address}`)
+//         return response.body.address.final_balance
+//     } catch(err) {
+//         return err
+//     }
+// }
+
+
 module.exports = {
     getDifficulty,
-    totalBtc
+    getBlockCount,
+    totalBtc,
+    bcPerBlock,
+    latestHash,
+    getWalletBalance
 }
